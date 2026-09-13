3ac4f2ea0ae056d888cbdd531e73dc84ff1beb30
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-09-12 22:52:25 -0300

    Login e Register completos

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-
+import Login from "./pages/Login";
+import { useEffect } from "react";
+import Register from "./pages/Register";
+import { supabase } from "./lib/supabase";
 import Dashboard from "./pages/Dashboard";
+import { useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+  // verifica se o usuário está logado
+  // useEffect(() => {
+  //   const verificarSession = async () => {
+  //     const { data } = await supabase.auth.getSession();
+
+  //     if (data.session) {
+  //       navigate("dashboard");
+  //     } else {
+  //       navigate("/login");
+  //     }
+  //   };
+  //   verificarSession();
+  // }, []);
+
   return (
-    <>
-      <Dashboard />
-      {/* <Login /> */}
-      {/* <Register /> */}
-    </>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
+import React from "react";
 import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    {" "}
-    <App />
-  </StrictMode>,
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
